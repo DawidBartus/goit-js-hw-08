8a2a4bc301c235cff0c7c30f3be507680e1fe7ff
Dawid <davidowsky9519@gmail.com>
2022-12-18 18:57:04 +0100

    ok

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+for (let el of galleryItems) {
+  let newLink = document.createElement('a');
+  newLink.classList.add('gallery__link');
+  newLink.href = el.original;
+
+  let newImg = document.createElement('img');
+  newImg.classList.add('gallery__image');
+  newImg.src = el.preview;
+  newImg.alt = el.description;
+
+  gallery.append(newLink);
+  newLink.append(newImg);
+}
+
+let lightboxElement = new SimpleLightbox('.gallery__link', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
