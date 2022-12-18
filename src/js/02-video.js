328a2ffcc9db47d286b826a26abfa55769ef82f4
Dawid <davidowsky9519@gmail.com>
2022-12-18 19:40:15 +0100

    form

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Vimeo from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const key = 'videoplayer-current-time';
+
+player.on('loaded', () => {
+  const time = localStorage.getItem(key) || 0;
+  player.setCurrentTime(time);
+});
+
+player.on(
+  'timeupdate',
+  _throttle(({ seconds }) => {
+    localStorage.setItem(key, seconds);
+    console.log(seconds);
+  }, 1000)
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,7 @@
+// const key = 'feedback-form-state';
+// const form = document.querySelectorAll('.feedback-form');
 
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+//   //   console.log(form.email.value);
+// });
