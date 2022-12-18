6f2c9ccd219248e2e59850d2e453f6c169d599dd
Dawid <davidowsky9519@gmail.com>
2022-12-18 20:17:49 +0100

    form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,32 @@
-// const key = 'feedback-form-state';
-// const form = document.querySelectorAll('.feedback-form');
+import _throttle from 'lodash.throttle';
 
-// form.addEventListener('submit', e => {
-//   e.preventDefault();
-//   //   console.log(form.email.value);
-// });
+const KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+let formStorage = {};
+
+form.addEventListener(
+  'input',
+  _throttle(e => {
+    formStorage[e.target.name] = e.target.value;
+    localStorage.setItem(KEY, JSON.stringify(formStorage));
+  }, 500)
+);
+
+let localSavedMessage = () => {
+  let formData = JSON.parse(localStorage.getItem(KEY));
+
+  if (formData.email) form.email.value = formData.email;
+  if (formData.message) form.message.value = formData.message;
+};
+localSavedMessage();
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  console.log(formStorage);
+  form.email.value = '';
+  form.message.value = '';
+  localStorage.clear();
+  formStorage = {};
+});
